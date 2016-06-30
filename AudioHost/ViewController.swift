315b597e5246d45673d013b5d9ce5fa8a4ca5192
armen <absoboll@gmail.com>
2016-06-29 23:26:23 -0700

    Initial Commit

---
+++ AudioHost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioHost
+//
+//  Created by armen karamian on 6/29/16.
+//  Copyright © 2016 armen karamian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
