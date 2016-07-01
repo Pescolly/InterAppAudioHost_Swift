f1441dde98e25aceef43b53a2d977a429ae08c08
armen <absoboll@gmail.com>
2016-06-30 22:55:02 -0700

    Inter app audio unit class complete.

---
+++ AudioHost/InterAppAudioUnit.swift
@@ -0,0 +1,19 @@
+//
+//  InterAppAudioUnit.swift
+//  AudioHost
+//
+//  Created by armen karamian on 6/30/16.
+//  Copyright © 2016 armen karamian. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+import AudioUnit
+import UIKit
+
+class InterAppAudioUnit : NSObject
+{
+	var name:String?
+	var compDescription:AudioComponentDescription?
+	var icon:UIImage?
+}
